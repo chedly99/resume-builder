3c9afc076918aa39607e20eafae255958d4b7a8f
chedly99 <mohamed.chedliiy@gmail.com>
2022-05-09 03:15:33 +0100

    wip4

--- client/src/pages/templates/index.js
+++ client/src/pages/templates/index.js
@@ -2,23 +2,12 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import DefaultLayout from '../../components/DefaultLayout'
 import Template1 from './Template1'
-import Template2 from './Template2'
 
 function Templates() {
-  const params = useParams()
-  const gettemplate=()=>{
-    switch(params.id){
-      case '1' : {
-        return <Template1/>
-      }
-      case '2' : {
-        return <Template2/>
-      }
-    }
-  }
+  
   return (
     <DefaultLayout>
-        {gettemplate()}
+        <Template1 />
     </DefaultLayout>
   )
 }
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const path = require('path')
 if(process.env.NODE_ENV=="production"){
     app.use("/", express.static("client/build"));
     app.get("*", (req,res) => {
-        res.sendFile(path.resolve(_dirname, "client/build/index.html"));
+        res.sendFile(path.resolve(__dirname, "client/build/index.html"));
     })
 }
 
